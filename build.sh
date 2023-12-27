1c2c1122c5bc8da06bc73d648a97e44419bbaeed
fengjiongmax <fengjiongmax@gmail.com>
2023-12-27 04:47:55 +0000

    use adapter-node for container and as default build target, add server hook for redirecting graphql request when using adapter-node

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# logic: check parameters passed to this script:
+# 1. if build for node, then do not touch anything, run `npm run build` directly
+# 2. if build for static, then overwrite `svelte.config.js` with `svelte-config/static-adapter.js`, then build
+
+TARGET=$1
+
+recoverConfig(){
+    # default build target is node
+    echo "recovering svelte build config"
+    cp svelte-configs/node-adapter.js svelte.config.js
+}
+
+if [ -z "$TARGET" ]; then
+    echo "Build target not provided, setting target to node"
+    TARGET="node"
+fi
+
+if [ "$TARGET" = "node" ]; then
+    echo "build for node"
+    npm run build
+elif [ "$TARGET" = "static" ]; then
+    echo "build for static"
+    npm run build-static
+    recoverConfig
+else
+    echo "Please provide correct build target"
+    echo "Available target: node, static"
+    echo "sh build.sh [target]"
+fi
