76a53b870dfd9287dd8628b1ba1ded7a9be6a2ad
fengjiongmax <fengjiongmax@gmail.com>
2023-12-30 07:15:24 +0000

    fix docker build failed

--- build-static.sh
+++ build-static.sh
@@ -4,5 +4,5 @@ echo "export const prerender = true;" > src/routes/+layout.ts
 cp svelte-configs/static-adapter.js svelte.config.js
 npm run build
 echo "recovering svelte build config"
-echo "export const prerender = true;export const ssr=false;" > src/routes/+layout.ts
+echo "export const ssr=false;" > src/routes/+layout.ts
 cp svelte-configs/node-adapter.js svelte.config.js
