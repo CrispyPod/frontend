af5c1ba3e4be749875e18aa92b7146184442e8e9
fengjiongmax <fengjiongmax@gmail.com>
2024-01-01 04:10:04 +0000

    add wrangler and upload command

--- build-static.sh
+++ build-static.sh
@@ -3,6 +3,7 @@ echo "build for static"
 echo "export const prerender = true;" > src/routes/+layout.ts
 cp svelte-configs/static-adapter.js svelte.config.js
 npm run build
+npx wrangler pages deploy build-static --project-name $CLOUDFLARE_PAGES_PROJECT_NAME
 echo "recovering svelte build config"
 echo "export const ssr=false;" > src/routes/+layout.ts
 cp svelte-configs/node-adapter.js svelte.config.js
