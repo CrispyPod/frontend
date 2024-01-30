4fb314ac4373de6d41d199237eba44f70834ab6d
fengjiongmax <fengjiongmax@gmail.com>
2024-01-30 09:20:01 +0000

    fix failed to generate static site

--- src/lib/serverFetch.ts
+++ src/lib/serverFetch.ts
@@ -4,18 +4,18 @@ import { serverGraphQLRequest } from "./serverGrqphQLRequest";
 import { siteConfigS } from "./stores/siteConfigStore";
 
 export async function FetchPagedEpisodes(pageIndex: number) {
-    let episodes: Array<Episode> | null = null;
-    let siteName: string = '';
-    let siteDescription: string = '';
+  let episodes: Array<Episode> | null = null;
+  let siteName: string = '';
+  let siteDescription: string = '';
 
-    let sum = 0;
-    let hasNextPage = false;
-    let hasPreviousPage = false;
+  let sum = 0;
+  let hasNextPage = false;
+  let hasPreviousPage = false;
 
 
-    let result = await serverGraphQLRequest(
-        null,
-        `query{
+  let result = await serverGraphQLRequest(
+    null,
+    `query{
         episodeList(pagination: {pageIndex: ${pageIndex}, perPage: 25}){
             items{
               id,
@@ -39,38 +39,38 @@ export async function FetchPagedEpisodes(pageIndex: number) {
                 defaultThumbnail
               }
             }`
-    );
+  );
 
-    let json_resp = await result.json();
+  let json_resp = await result.json();
 
-    hasPreviousPage = json_resp.data.episodeList.pageInfo.hasPreviousPage ?? false;
-    hasNextPage = json_resp.data.episodeList.pageInfo.hasNextPage ?? false;
-    sum = json_resp.data.episodeList.totalCount ?? 0;
+  hasPreviousPage = json_resp.data.episodeList.pageInfo.hasPreviousPage ?? false;
+  hasNextPage = json_resp.data.episodeList.pageInfo.hasNextPage ?? false;
+  sum = json_resp.data.episodeList.totalCount ?? 0;
 
-    siteName = json_resp.data.siteConfig.siteName;
-    siteDescription = json_resp.data.siteConfig.siteDescription;
-    let siteUrl = json_resp.data.siteConfig.siteUrl;
+  siteName = json_resp.data.siteConfig.siteName;
+  siteDescription = json_resp.data.siteConfig.siteDescription;
+  let siteUrl = json_resp.data.siteConfig.siteUrl;
 
-    siteConfigS.set({
-        ...json_resp.data.siteConfig
-    });
+  siteConfigS.set({
+    ...json_resp.data.siteConfig
+  });
 
-    episodes = json_resp.data.episodeList.items;
-    episodes?.forEach((e) => {
-        if (e.thumbnailFileName != null) {
-            e.thumbnailFileName = getSiteUrlPrefix() + '/api/imageFile/' + e.thumbnailFileName;
-        } else {
-            e.thumbnailFileName = json_resp.data.siteConfig.defaultThumbnail.length > 0 ? `/api/imageFile/${json_resp.data.siteConfig.defaultThumbnail}` : '/EpisodeDefaultThumbnailSquare.png';
-        }
-    })
+  episodes = json_resp.data.episodeList.items;
+  episodes?.forEach((e) => {
+    if (e.thumbnailFileName != null) {
+      e.thumbnailFileName = getSiteUrlPrefix() + '/api/imageFile/' + e.thumbnailFileName;
+    } else {
+      e.thumbnailFileName = json_resp.data.siteConfig.defaultThumbnail.length > 0 ? getSiteUrlPrefix() + `/api/imageFile/${json_resp.data.siteConfig.defaultThumbnail}` : '/EpisodeDefaultThumbnailSquare.png';
+    }
+  })
 
-    return {
-        episodes,
-        siteName,
-        siteDescription,
-        siteUrl,
-        sum,
-        hasNextPage,
-        hasPreviousPage,
-    };
+  return {
+    episodes,
+    siteName,
+    siteDescription,
+    siteUrl,
+    sum,
+    hasNextPage,
+    hasPreviousPage,
+  };
 }
