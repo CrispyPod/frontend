896a47c958b72ae2420fb9be4b9c3a9e5511c72b
fengjiongmax <fengjiongmax@gmail.com>
2024-05-19 10:38:13 +0000

    remove admin-hook and fix some components

--- src/lib/components/EpisodeDetailAudio.svelte
+++ src/lib/components/EpisodeDetailAudio.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
+	import { PUBLIC_PB_ENDPOINT } from '$env/static/public';
 	import type { Episode } from '$lib/models/episode';
+	import { COLLECTION_EPISODE } from '$lib/pb-integrate/pb_client';
 	import WaveForm from './WaveForm.svelte';
 
 	export let episodeData: Episode;
 </script>
 
 {#if episodeData != null}
-	{#if episodeData.audioFileName != null && episodeData.audioFileName.length > 0}
+	{#if episodeData.audio_file != null && episodeData.audio_file.length > 0}
 		<div class="card lg:card-side bg-base-100 shadow-xl my-10 mr-6">
 			<figure>
 				<img
 					class="w-80 h-80"
-					src={`/api/imageFile/${episodeData.thumbnailFileName}`}
+					src={`${PUBLIC_PB_ENDPOINT}api/files/${COLLECTION_EPISODE}/${episodeData.id}/${episodeData.thumbnail}`}
 					alt={episodeData.title}
 				/>
 			</figure>
 
 			<div class="card-body">
-				<WaveForm fileUrl={`/api/audioFile/${episodeData.audioFileName}`} />
+				<WaveForm
+					fileUrl={`${PUBLIC_PB_ENDPOINT}api/files/${COLLECTION_EPISODE}/${episodeData.id}/${episodeData.audio_file}`}
+				/>
 			</div>
 		</div>
 	{/if}
--- src/lib/components/admin-hooks/HookStatusBadge.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let hookStatus: number;
-</script>
-
-{#if hookStatus == 0}
-	<div class="badge badge-neutral">running</div>
-{:else if hookStatus == 1}
-	<div class="badge badge-accent">finished</div>
-{/if}
