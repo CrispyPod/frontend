550d8f0829fa1e7127203abc798ccf38d996f85e
fengjiongmax <fengjiongmax@gmail.com>
2024-09-22 10:43:29 +0000

    admin siteconfig store use '/' as pocketbase endpoint

--- src/lib/pb-integrate/admin_pb.ts
+++ src/lib/pb-integrate/admin_pb.ts
@@ -1,8 +1,7 @@
-import { PUBLIC_PB_ENDPOINT } from "$env/static/public";
 import PocketBase from 'pocketbase';
 
 // on /admin/*, we will all use front end rendering, and for the rest will use ssr
-export const backend_pb = new PocketBase(PUBLIC_PB_ENDPOINT);
+export const backend_pb = new PocketBase("/");
 
 export const COLLECTION_USER = "users"
 export const COLLECTION_EPISODE = "episodes"
