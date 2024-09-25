9564cd82722a40847250acda12d1cdf868f6087b
fengjiongmax <fengjiongmax@gmail.com>
2024-09-25 09:29:53 +0000

    upload to cf pages using main branch

--- static-deploy.sh
+++ static-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-npx wrangler pages deploy build-static --project-name $CLOUDFLARE_PAGES_PROJECT_NAME
+npx wrangler pages deploy build-static --project-name $CLOUDFLARE_PAGES_PROJECT_NAME --branch main
 # echo "recovering svelte build config"
 # cp svelte-configs/node-adapter.js svelte.config.js
